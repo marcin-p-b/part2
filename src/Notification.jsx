c5d08246b572974d97853503a138738d1467bbd5
marcin-p-b <162914885+marcin-p-b@users.noreply.github.com>
2024-05-10 19:46:31 +0200

    Delete src directory

--- src/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Notification({ message }) {
-  if (message === "") {
-    return null;
-  }
-
-  return <div className="error">{message}</div>;
-}
